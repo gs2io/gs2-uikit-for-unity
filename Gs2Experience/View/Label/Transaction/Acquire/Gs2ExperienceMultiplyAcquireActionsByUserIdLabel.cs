6171352db738744cdbe26099a3fc4a13fd51bb89
gs2-ci <ci@gs2.io>
2023-08-02 16:14:23 +0000

    607ef8e895d0f780df06ab24414c3998b8822ce1

---
+++ Gs2Experience/View/Label/Transaction/Acquire/Gs2ExperienceMultiplyAcquireActionsByUserIdLabel.cs
@@ -0,0 +1,186 @@
+/*
+ * Copyright 2016 Game Server Services, Inc. or its affiliates. All Rights
+ * Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable CheckNamespace
+// ReSharper disable RedundantNameQualifier
+// ReSharper disable RedundantAssignment
+// ReSharper disable NotAccessedVariable
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable Unity.NoNullPropagation
+// ReSharper disable InconsistentNaming
+
+#pragma warning disable CS0472
+
+using System;
+using Gs2.Gs2Experience.Request;
+using Gs2.Unity.UiKit.Core;
+using Gs2.Unity.UiKit.Gs2Core.Fetcher;
+using Gs2.Unity.UiKit.Gs2Experience.Fetcher;
+using Gs2.Util.LitJson;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Gs2.Unity.UiKit.Gs2Experience.Label
+{
+    /// <summary>
+    /// Main
+    /// </summary>
+
+	[AddComponentMenu("GS2 UIKit/Experience/Status/View/Label/Transaction/Gs2ExperienceMultiplyAcquireActionsByUserIdLabel")]
+    public partial class Gs2ExperienceMultiplyAcquireActionsByUserIdLabel : MonoBehaviour
+    {
+        public void Update()
+        {
+            if (_fetcher.Fetched && _fetcher.Request != null &&
+                    _userDataFetcher != null && _userDataFetcher.Fetched && _userDataFetcher.Status != null) {
+                {
+                    onUpdate?.Invoke(
+                        format.Replace(
+                            "{namespaceName}",
+                            $"{_fetcher.Request.NamespaceName}"
+                        ).Replace(
+                            "{userId}",
+                            $"{_fetcher.Request.UserId}"
+                        ).Replace(
+                            "{experienceName}",
+                            $"{_fetcher.Request.ExperienceName}"
+                        ).Replace(
+                            "{propertyId}",
+                            $"{_fetcher.Request.PropertyId}"
+                        ).Replace(
+                            "{rateName}",
+                            $"{_fetcher.Request.RateName}"
+                        ).Replace(
+                            "{acquireActions}",
+                            $"{_fetcher.Request.AcquireActions}"
+                        ).Replace(
+                            "{userData:experienceName}",
+                            $"{_userDataFetcher.Status.ExperienceName}"
+                        ).Replace(
+                            "{userData:propertyId}",
+                            $"{_userDataFetcher.Status.PropertyId}"
+                        ).Replace(
+                            "{userData:experienceValue}",
+                            $"{_userDataFetcher.Status.ExperienceValue}"
+                        ).Replace(
+                            "{userData:rankValue}",
+                            $"{_userDataFetcher.Status.RankValue}"
+                        ).Replace(
+                            "{userData:rankCapValue}",
+                            $"{_userDataFetcher.Status.RankCapValue}"
+                        )
+                    );
+                }
+            } else if (_fetcher.Fetched && _fetcher.Request != null) {
+                {
+                    onUpdate?.Invoke(
+                        format.Replace(
+                            "{namespaceName}",
+                            $"{_fetcher.Request.NamespaceName}"
+                        ).Replace(
+                            "{userId}",
+                            $"{_fetcher.Request.UserId}"
+                        ).Replace(
+                            "{experienceName}",
+                            $"{_fetcher.Request.ExperienceName}"
+                        ).Replace(
+                            "{propertyId}",
+                            $"{_fetcher.Request.PropertyId}"
+                        ).Replace(
+                            "{rateName}",
+                            $"{_fetcher.Request.RateName}"
+                        ).Replace(
+                            "{acquireActions}",
+                            $"{_fetcher.Request.AcquireActions}"
+                        )
+                    );
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Dependent components
+    /// </summary>
+
+    public partial class Gs2ExperienceMultiplyAcquireActionsByUserIdLabel
+    {
+        private Gs2ExperienceMultiplyAcquireActionsByUserIdFetcher _fetcher;
+        private Gs2ExperienceOwnStatusFetcher _userDataFetcher;
+
+        public void Awake()
+        {
+            _fetcher = GetComponent<Gs2ExperienceMultiplyAcquireActionsByUserIdFetcher>() ?? GetComponentInParent<Gs2ExperienceMultiplyAcquireActionsByUserIdFetcher>();
+            _userDataFetcher = GetComponent<Gs2ExperienceOwnStatusFetcher>() ?? GetComponentInParent<Gs2ExperienceOwnStatusFetcher>();
+
+            if (_fetcher == null) {
+                Debug.LogError($"{gameObject.GetFullPath()}: Couldn't find the Gs2ExperienceMultiplyAcquireActionsByUserIdFetcher.");
+                enabled = false;
+            }
+
+            Update();
+        }
+
+        public bool HasError()
+        {
+            _fetcher = GetComponent<Gs2ExperienceMultiplyAcquireActionsByUserIdFetcher>() ?? GetComponentInParent<Gs2ExperienceMultiplyAcquireActionsByUserIdFetcher>(true);
+            _userDataFetcher = GetComponent<Gs2ExperienceOwnStatusFetcher>() ?? GetComponentInParent<Gs2ExperienceOwnStatusFetcher>(true);
+            if (_fetcher == null) {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Public properties
+    /// </summary>
+
+    public partial class Gs2ExperienceMultiplyAcquireActionsByUserIdLabel
+    {
+
+    }
+
+    /// <summary>
+    /// Parameters for Inspector
+    /// </summary>
+
+    public partial class Gs2ExperienceMultiplyAcquireActionsByUserIdLabel
+    {
+        public string format;
+    }
+
+    /// <summary>
+    /// Event handlers
+    /// </summary>
+    public partial class Gs2ExperienceMultiplyAcquireActionsByUserIdLabel
+    {
+        [Serializable]
+        private class UpdateEvent : UnityEvent<string>
+        {
+
+        }
+
+        [SerializeField]
+        private UpdateEvent onUpdate = new UpdateEvent();
+
+        public event UnityAction<string> OnUpdate
+        {
+            add => onUpdate.AddListener(value);
+            remove => onUpdate.RemoveListener(value);
+        }
+    }
+}
