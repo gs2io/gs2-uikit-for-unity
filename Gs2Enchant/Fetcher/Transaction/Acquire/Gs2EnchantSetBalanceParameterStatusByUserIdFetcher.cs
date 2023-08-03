ee336ecb78bd15d039242453e742925de300867b
gs2-ci <ci@gs2.io>
2023-08-03 06:35:22 +0000

    e88c506ea36d24cba8877551e2f5c82ce2a02014

---
+++ Gs2Enchant/Fetcher/Transaction/Acquire/Gs2EnchantSetBalanceParameterStatusByUserIdFetcher.cs
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2016 Game Server Services, Inc. or its affiliates. All Rights
+ * Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable CheckNamespace
+// ReSharper disable RedundantNameQualifier
+// ReSharper disable RedundantAssignment
+// ReSharper disable NotAccessedVariable
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable Unity.NoNullPropagation
+// ReSharper disable InconsistentNaming
+
+#pragma warning disable CS0472
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Gs2.Core.Exception;
+using Gs2.Gs2Enchant.Request;
+using Gs2.Unity.Gs2Enchant.ScriptableObject;
+using Gs2.Unity.UiKit.Core;
+using Gs2.Unity.UiKit.Gs2Core.Fetcher;
+using Gs2.Unity.UiKit.Gs2Enchant.Context;
+using Gs2.Unity.UiKit.Gs2Enchant.Fetcher;
+using Gs2.Unity.Util;
+using Gs2.Util.LitJson;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Gs2.Unity.UiKit.Gs2Enchant.Fetcher
+{
+    /// <summary>
+    /// Main
+    /// </summary>
+
+	[AddComponentMenu("GS2 UIKit/Enchant/BalanceParameterStatus/Fetcher/Acquire/Gs2EnchantSetBalanceParameterStatusByUserIdFetcher")]
+    public partial class Gs2EnchantSetBalanceParameterStatusByUserIdFetcher : Gs2EnchantOwnBalanceParameterStatusContext
+    {
+        private IEnumerator Fetch()
+        {
+            while (true)
+            {
+                if (_fetcher != null) {
+                    var action = _fetcher.AcquireActions().FirstOrDefault(v => v.Action == "Gs2Enchant:SetBalanceParameterStatusByUserId");
+                    if (action != null) {
+                        Request = SetBalanceParameterStatusByUserIdRequest.FromJson(JsonMapper.ToObject(action.Request));
+                        if (BalanceParameterStatus == null || (
+                                BalanceParameterStatus.NamespaceName == Request.NamespaceName &&
+                                BalanceParameterStatus.ParameterName == Request.ParameterName &&
+                                BalanceParameterStatus.PropertyId == Request.PropertyId)
+                           ) {
+                            BalanceParameterStatus = OwnBalanceParameterStatus.New(
+                                Namespace.New(
+                                    Request.NamespaceName
+                                ),
+                                Request.ParameterName,
+                                Request.PropertyId
+                            );
+                        }
+                        Fetched = true;
+                    }
+                }
+                yield return new WaitForSeconds(0.1f);
+            }
+            // ReSharper disable once IteratorNeverReturns
+        }
+
+        public void OnEnable()
+        {
+            StartCoroutine(nameof(Fetch));
+        }
+
+        public void OnDisable()
+        {
+            StopCoroutine(nameof(Fetch));
+        }
+    }
+
+    /// <summary>
+    /// Dependent components
+    /// </summary>
+
+    public partial class Gs2EnchantSetBalanceParameterStatusByUserIdFetcher
+    {
+        private IAcquireActionsFetcher _fetcher;
+
+        public new void Start() {
+
+        }
+
+        public void Awake()
+        {
+            _fetcher = GetComponent<IAcquireActionsFetcher>() ?? GetComponentInParent<IAcquireActionsFetcher>();
+
+            if (_fetcher == null) {
+                Debug.LogError($"{gameObject.GetFullPath()}: Couldn't find the IAcquireActionFetcher.");
+                enabled = false;
+            }
+        }
+
+        public bool HasError()
+        {
+            _fetcher = GetComponent<IAcquireActionsFetcher>() ?? GetComponentInParent<IAcquireActionsFetcher>(true);
+            if (_fetcher == null) {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Public properties
+    /// </summary>
+
+    public partial class Gs2EnchantSetBalanceParameterStatusByUserIdFetcher
+    {
+        public SetBalanceParameterStatusByUserIdRequest Request { get; protected set; }
+        public bool Fetched { get; protected set; }
+    }
+
+    /// <summary>
+    /// Parameters for Inspector
+    /// </summary>
+
+    public partial class Gs2EnchantSetBalanceParameterStatusByUserIdFetcher
+    {
+
+    }
+
+    /// <summary>
+    /// Event handlers
+    /// </summary>
+    public partial class Gs2EnchantSetBalanceParameterStatusByUserIdFetcher
+    {
+        [SerializeField]
+        internal ErrorEvent onError = new ErrorEvent();
+
+        public event UnityAction<Gs2Exception, Func<IEnumerator>> OnError
+        {
+            add => onError.AddListener(value);
+            remove => onError.RemoveListener(value);
+        }
+    }
+}
---
+++ Gs2Enchant/View/Label/Transaction/Acquire/Gs2EnchantSetBalanceParameterStatusByUserIdLabel.cs
@@ -0,0 +1,174 @@
+/*
+ * Copyright 2016 Game Server Services, Inc. or its affiliates. All Rights
+ * Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable CheckNamespace
+// ReSharper disable RedundantNameQualifier
+// ReSharper disable RedundantAssignment
+// ReSharper disable NotAccessedVariable
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable Unity.NoNullPropagation
+// ReSharper disable InconsistentNaming
+
+#pragma warning disable CS0472
+
+using System;
+using Gs2.Gs2Enchant.Request;
+using Gs2.Unity.UiKit.Core;
+using Gs2.Unity.UiKit.Gs2Core.Fetcher;
+using Gs2.Unity.UiKit.Gs2Enchant.Fetcher;
+using Gs2.Util.LitJson;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Gs2.Unity.UiKit.Gs2Enchant.Label
+{
+    /// <summary>
+    /// Main
+    /// </summary>
+
+	[AddComponentMenu("GS2 UIKit/Enchant/BalanceParameterStatus/View/Label/Transaction/Gs2EnchantSetBalanceParameterStatusByUserIdLabel")]
+    public partial class Gs2EnchantSetBalanceParameterStatusByUserIdLabel : MonoBehaviour
+    {
+        public void Update()
+        {
+            if (_fetcher.Fetched && _fetcher.Request != null &&
+                    _userDataFetcher != null && _userDataFetcher.Fetched && _userDataFetcher.BalanceParameterStatus != null) {
+                {
+                    onUpdate?.Invoke(
+                        format.Replace(
+                            "{namespaceName}",
+                            $"{_fetcher.Request.NamespaceName}"
+                        ).Replace(
+                            "{userId}",
+                            $"{_fetcher.Request.UserId}"
+                        ).Replace(
+                            "{parameterName}",
+                            $"{_fetcher.Request.ParameterName}"
+                        ).Replace(
+                            "{propertyId}",
+                            $"{_fetcher.Request.PropertyId}"
+                        ).Replace(
+                            "{parameterValues}",
+                            $"{_fetcher.Request.ParameterValues}"
+                        ).Replace(
+                            "{userData:parameterName}",
+                            $"{_userDataFetcher.BalanceParameterStatus.ParameterName}"
+                        ).Replace(
+                            "{userData:propertyId}",
+                            $"{_userDataFetcher.BalanceParameterStatus.PropertyId}"
+                        ).Replace(
+                            "{userData:parameterValues}",
+                            $"{_userDataFetcher.BalanceParameterStatus.ParameterValues}"
+                        )
+                    );
+                }
+            } else if (_fetcher.Fetched && _fetcher.Request != null) {
+                {
+                    onUpdate?.Invoke(
+                        format.Replace(
+                            "{namespaceName}",
+                            $"{_fetcher.Request.NamespaceName}"
+                        ).Replace(
+                            "{userId}",
+                            $"{_fetcher.Request.UserId}"
+                        ).Replace(
+                            "{parameterName}",
+                            $"{_fetcher.Request.ParameterName}"
+                        ).Replace(
+                            "{propertyId}",
+                            $"{_fetcher.Request.PropertyId}"
+                        ).Replace(
+                            "{parameterValues}",
+                            $"{_fetcher.Request.ParameterValues}"
+                        )
+                    );
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Dependent components
+    /// </summary>
+
+    public partial class Gs2EnchantSetBalanceParameterStatusByUserIdLabel
+    {
+        private Gs2EnchantSetBalanceParameterStatusByUserIdFetcher _fetcher;
+        private Gs2EnchantOwnBalanceParameterStatusFetcher _userDataFetcher;
+
+        public void Awake()
+        {
+            _fetcher = GetComponent<Gs2EnchantSetBalanceParameterStatusByUserIdFetcher>() ?? GetComponentInParent<Gs2EnchantSetBalanceParameterStatusByUserIdFetcher>();
+            _userDataFetcher = GetComponent<Gs2EnchantOwnBalanceParameterStatusFetcher>() ?? GetComponentInParent<Gs2EnchantOwnBalanceParameterStatusFetcher>();
+
+            if (_fetcher == null) {
+                Debug.LogError($"{gameObject.GetFullPath()}: Couldn't find the Gs2EnchantSetBalanceParameterStatusByUserIdFetcher.");
+                enabled = false;
+            }
+
+            Update();
+        }
+
+        public bool HasError()
+        {
+            _fetcher = GetComponent<Gs2EnchantSetBalanceParameterStatusByUserIdFetcher>() ?? GetComponentInParent<Gs2EnchantSetBalanceParameterStatusByUserIdFetcher>(true);
+            _userDataFetcher = GetComponent<Gs2EnchantOwnBalanceParameterStatusFetcher>() ?? GetComponentInParent<Gs2EnchantOwnBalanceParameterStatusFetcher>(true);
+            if (_fetcher == null) {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Public properties
+    /// </summary>
+
+    public partial class Gs2EnchantSetBalanceParameterStatusByUserIdLabel
+    {
+
+    }
+
+    /// <summary>
+    /// Parameters for Inspector
+    /// </summary>
+
+    public partial class Gs2EnchantSetBalanceParameterStatusByUserIdLabel
+    {
+        public string format;
+    }
+
+    /// <summary>
+    /// Event handlers
+    /// </summary>
+    public partial class Gs2EnchantSetBalanceParameterStatusByUserIdLabel
+    {
+        [Serializable]
+        private class UpdateEvent : UnityEvent<string>
+        {
+
+        }
+
+        [SerializeField]
+        private UpdateEvent onUpdate = new UpdateEvent();
+
+        public event UnityAction<string> OnUpdate
+        {
+            add => onUpdate.AddListener(value);
+            remove => onUpdate.RemoveListener(value);
+        }
+    }
+}
