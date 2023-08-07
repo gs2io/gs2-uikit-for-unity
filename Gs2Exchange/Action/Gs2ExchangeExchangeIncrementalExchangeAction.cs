21e31b79b5330bbb369ef334208143c387c2ada9
gs2-ci <ci@gs2.io>
2023-08-07 10:57:36 +0000

    69bdeebbc12d1d01d5eb9ad2e686c5b1edd7aa09

---
+++ Gs2Exchange/Action/Gs2ExchangeExchangeIncrementalExchangeAction.cs
@@ -0,0 +1,272 @@
+/*
+ * Copyright 2016 Game Server Services, Inc. or its affiliates. All Rights
+ * Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Local
+// ReSharper disable CheckNamespace
+// ReSharper disable RedundantNameQualifier
+// ReSharper disable RedundantAssignment
+// ReSharper disable NotAccessedVariable
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable Unity.NoNullPropagation
+// ReSharper disable InconsistentNaming
+
+#pragma warning disable CS0472
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Gs2.Core.Exception;
+using Gs2.Unity.Gs2Exchange.Model;
+using Gs2.Unity.Util;
+using Gs2.Unity.UiKit.Core;
+using Gs2.Unity.UiKit.Gs2Exchange.Context;
+using UnityEngine;
+using UnityEngine.Events;
+using User = Gs2.Unity.Gs2Exchange.ScriptableObject.User;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Gs2.Unity.UiKit.Gs2Exchange
+{
+    public partial class Gs2ExchangeExchangeIncrementalExchangeAction : MonoBehaviour
+    {
+        private IEnumerator Process()
+        {
+            yield return new WaitUntil(() => this._clientHolder.Initialized);
+            yield return new WaitUntil(() => this._gameSessionHolder.Initialized);
+            
+            var domain = this._clientHolder.Gs2.Exchange.Namespace(
+                this._context.Namespace.NamespaceName
+            ).Me(
+                this._gameSessionHolder.GameSession
+            ).Exchange(
+            );
+            var future = domain.IncrementalExchange(
+                RateName,
+                Count,
+                Config.ToArray()
+            );
+            yield return future;
+            if (future.Error != null)
+            {
+                if (future.Error is TransactionException e)
+                {
+                    IEnumerator Retry()
+                    {
+                        var retryFuture = e.Retry();
+                        yield return retryFuture;
+                        if (retryFuture.Error != null)
+                        {
+                            this.onError.Invoke(future.Error, Retry);
+                            yield break;
+                        }
+                        this.onIncrementalExchangeComplete.Invoke(future.Result.TransactionId);
+                    }
+
+                    this.onError.Invoke(future.Error, Retry);
+                    yield break;
+                }
+
+                this.onError.Invoke(future.Error, null);
+                yield break;
+            }
+            this.onIncrementalExchangeComplete.Invoke(future.Result.TransactionId);
+        }
+
+        public void OnEnable()
+        {
+            StartCoroutine(nameof(Process));
+        }
+
+        public void OnDisable()
+        {
+            StopCoroutine(nameof(Process));
+        }
+    }
+
+    /// <summary>
+    /// Dependent components
+    /// </summary>
+
+    public partial class Gs2ExchangeExchangeIncrementalExchangeAction
+    {
+        private Gs2ClientHolder _clientHolder;
+        private Gs2GameSessionHolder _gameSessionHolder;
+        private Gs2ExchangeNamespaceContext _context;
+
+        public void Awake()
+        {
+            this._clientHolder = Gs2ClientHolder.Instance;
+            this._gameSessionHolder = Gs2GameSessionHolder.Instance;
+            this._context = GetComponent<Gs2ExchangeNamespaceContext>() ?? GetComponentInParent<Gs2ExchangeNamespaceContext>();
+
+            if (_context == null) {
+                Debug.LogError($"{gameObject.GetFullPath()}: Couldn't find the Gs2ExchangeNamespaceContext.");
+                enabled = false;
+            }
+        }
+
+        public bool HasError()
+        {
+            this._context = GetComponent<Gs2ExchangeNamespaceContext>() ?? GetComponentInParent<Gs2ExchangeNamespaceContext>(true);
+            if (_context == null) {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Public properties
+    /// </summary>
+
+    public partial class Gs2ExchangeExchangeIncrementalExchangeAction
+    {
+
+    }
+
+    /// <summary>
+    /// Parameters for Inspector
+    /// </summary>
+    public partial class Gs2ExchangeExchangeIncrementalExchangeAction
+    {
+        public string RateName;
+        public int Count;
+        public List<Gs2.Unity.Gs2Exchange.Model.EzConfig> Config;
+
+        public void SetRateName(string value) {
+            RateName = value;
+            this.onChangeRateName.Invoke(RateName);
+        }
+
+        public void SetCount(int value) {
+            Count = value;
+            this.onChangeCount.Invoke(Count);
+        }
+
+        public void DecreaseCount() {
+            Count -= 1;
+            this.onChangeCount.Invoke(Count);
+        }
+
+        public void IncreaseCount() {
+            Count += 1;
+            this.onChangeCount.Invoke(Count);
+        }
+
+        public void SetConfig(List<Gs2.Unity.Gs2Exchange.Model.EzConfig> value) {
+            Config = value;
+            this.onChangeConfig.Invoke(Config);
+        }
+    }
+
+    /// <summary>
+    /// Event handlers
+    /// </summary>
+    public partial class Gs2ExchangeExchangeIncrementalExchangeAction
+    {
+
+        [Serializable]
+        private class ChangeRateNameEvent : UnityEvent<string>
+        {
+
+        }
+
+        [SerializeField]
+        private ChangeRateNameEvent onChangeRateName = new ChangeRateNameEvent();
+        public event UnityAction<string> OnChangeRateName
+        {
+            add => this.onChangeRateName.AddListener(value);
+            remove => this.onChangeRateName.RemoveListener(value);
+        }
+
+        [Serializable]
+        private class ChangeCountEvent : UnityEvent<int>
+        {
+
+        }
+
+        [SerializeField]
+        private ChangeCountEvent onChangeCount = new ChangeCountEvent();
+        public event UnityAction<int> OnChangeCount
+        {
+            add => this.onChangeCount.AddListener(value);
+            remove => this.onChangeCount.RemoveListener(value);
+        }
+
+        [Serializable]
+        private class ChangeConfigEvent : UnityEvent<List<Gs2.Unity.Gs2Exchange.Model.EzConfig>>
+        {
+
+        }
+
+        [SerializeField]
+        private ChangeConfigEvent onChangeConfig = new ChangeConfigEvent();
+        public event UnityAction<List<Gs2.Unity.Gs2Exchange.Model.EzConfig>> OnChangeConfig
+        {
+            add => this.onChangeConfig.AddListener(value);
+            remove => this.onChangeConfig.RemoveListener(value);
+        }
+
+        [Serializable]
+        private class IncrementalExchangeCompleteEvent : UnityEvent<string>
+        {
+
+        }
+
+        [SerializeField]
+        private IncrementalExchangeCompleteEvent onIncrementalExchangeComplete = new IncrementalExchangeCompleteEvent();
+        public event UnityAction<string> OnIncrementalExchangeComplete
+        {
+            add => this.onIncrementalExchangeComplete.AddListener(value);
+            remove => this.onIncrementalExchangeComplete.RemoveListener(value);
+        }
+
+        [SerializeField]
+        internal ErrorEvent onError = new ErrorEvent();
+
+        public event UnityAction<Gs2Exception, Func<IEnumerator>> OnError
+        {
+            add => this.onError.AddListener(value);
+            remove => this.onError.RemoveListener(value);
+        }
+    }
+
+#if UNITY_EDITOR
+
+    /// <summary>
+    /// Context Menu
+    /// </summary>
+    public partial class Gs2ExchangeExchangeIncrementalExchangeAction
+    {
+        [MenuItem("GameObject/Game Server Services/Exchange/Exchange/Action/IncrementalExchange", priority = 0)]
+        private static void CreateButton()
+        {
+            var prefab = AssetDatabase.LoadAssetAtPath<Gs2ExchangeExchangeIncrementalExchangeAction>(
+                "Packages/io.gs2.unity.sdk.uikit/Gs2Exchange/Prefabs/Action/Gs2ExchangeExchangeIncrementalExchangeAction.prefab"
+            );
+
+            var instance = PrefabUtility.InstantiatePrefab(prefab, Selection.activeTransform);
+
+            Undo.RegisterCreatedObjectUndo(instance, $"Create {instance.name}");
+            Selection.activeObject = instance;
+        }
+    }
+#endif
+}
